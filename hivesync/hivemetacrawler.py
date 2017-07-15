e5d5a6483c6cdf2a13592acd856a735bf6704d81
EC2 Default User <ec2-user@ip-172-31-10-252.ap-northeast-1.compute.internal>
2017-07-15 16:23:25 +0800

    add get_tables function

--- hivesync/hivemetacrawler.py
+++ hivesync/hivemetacrawler.py
@@ -4,13 +4,29 @@ class HiveMetaCrawler():
     def __init__(self, *args, **kwargs):
         self.conn = connect(auth_mechanism='PLAIN', *args, **kwargs)
         self.cursor = self.conn.cursor()
-        self.meta = {}
+        self.meta = None
         
     def get_databases(self):
-        self.cursor.execute("show databases")
-        result = self.cursor.fetchall()
-        for db in result:
-            if db[0] not in self.meta.keys():
-                self.meta[db[0]] = {}
-        
+        if not self.meta:
+            self.meta={}
+            self.cursor.execute("show databases")
+            result = self.cursor.fetchall()
+            for db in result:
+                if db[0] not in self.meta.keys():
+                    self.meta[db[0]] = {}
         return self.meta
+
+    def get_tables(self, db):
+        if db not in self.meta.keys():
+            self.get_databases()
+            if db not in self.meta.keys():
+                raise ValueError("There is no database named {}.".format(db))
+        
+        self.cursor.execute("use {}".format(db))
+        self.cursor.execute("show tables")
+        result = self.cursor.fetchall()
+        for table in result:
+            self.meta[db][table[0]] = {}
+
+        return self.meta[db]
+
