b8aba78a4d47a14254cf672ae9775989b03941f0
bryanyang0528 <ec2-user@ip-172-31-10-252.ap-northeast-1.compute.internal>
2017-07-17 12:00:06 +0800

    refactor

--- hivesync/hivemetacrawler.py
+++ hivesync/hivemetacrawler.py
@@ -4,32 +4,38 @@ class HiveMetaCrawler():
     def __init__(self, *args, **kwargs):
         self.conn = connect(auth_mechanism='PLAIN', *args, **kwargs)
         self.cursor = self.conn.cursor()
-        self.meta = None
-    
-    @property
-    def get_databases(self):
-        if not self.meta:
-            self.meta={}
-            self.cursor.execute("show databases")
-            result = self.cursor.fetchall()
-            for db in result:
-                if db[0] not in self.meta.keys():
-                    self.meta[db[0]] = {}
-        return self.meta
+        self.meta = self._get_all_tables
 
-    def get_tables(self, db):
-        if db not in self.meta.keys():
-            self.get_databases()
-            if db not in self.meta.keys():
-                raise ValueError("There is no database named {}.".format(db))
-        
+    @property
+    def _get_databases(self):
+        meta={}
+        self.cursor.execute("show databases")
+        result = self.cursor.fetchall()
+        for db in result:
+            if db[0] not in meta.keys():
+                meta[db[0]] = {}
+
+        return meta
+
+    def get_databases(self):
+        return self.meta.keys()
+
+    def _get_tables(self, db):
+        tables = []
         self.cursor.execute("use {}".format(db))
         self.cursor.execute("show tables")
         result = self.cursor.fetchall()
+        
         for table in result:
-            self.meta[db][table[0]] = {}
+            tables.append(table[0])
+        
+        return tables
 
-        return self.meta[db]
+    def get_tables(self, db):
+        if db not in self._get_databases:
+            raise ValueError("There is no database named {}.".format(db))
+        else:
+            return self.meta[db].keys()
 
     def get_create_table_script(self, db, table):
         self.cursor.execute("SHOW CREATE TABLE {}.{}".format(db, table))
@@ -39,25 +45,35 @@ class HiveMetaCrawler():
             if 'TBLPROPERTIES' in line[0]:
                 break
             else:
-                script.extend(line[0])
-            
+                script.append(line[0])
+        
         script=''.join(script)
         self.meta[db][table] = script
 
         return self.meta[db][table]
 
     @property
-    def get_all_tables(self):
-        self.get_databases
-        for db in self.meta.keys():
-            self.get_tables(db)
-        
-        return self.meta
+    def _get_all_tables(self):
+        meta = {}
+        for db in self._get_databases:
+            meta[db] = {}
+            tables = self._get_tables(db)
+            for table in tables:
+                meta[db][table] = {}
+
+        return meta
 
     @property
-    def get_all_create_table_scripts(self):
-        self.get_databases
-        self.get_all_tables
+    def get_all_tables(self):
+        result = {}
+        for db in self.meta.keys():
+            result[db] = []
+            for table in self.meta[db].keys():
+                result[db].append(table)
+        return result
+
+    @property
+    def _get_all_create_table_scripts(self):
         for db in self.meta.keys():
             for table in self.meta[db].keys():
                 self.get_create_table_script(db, table)
