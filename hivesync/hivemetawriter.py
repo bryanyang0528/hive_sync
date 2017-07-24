22e4bef1b4360e49e7f232213a4a594c44d66e5d
EC2 Default User <ec2-user@ip-172-31-1-175.ap-northeast-1.compute.internal>
2017-07-24 14:02:58 +0800

    add repair table function

--- hivesync/hivemetawriter.py
+++ hivesync/hivemetawriter.py
@@ -1,25 +1,29 @@
 from impala.dbapi import connect
 from hivesync.hivemetacrawler import HiveMetaCrawler
+import time
 
 class HiveMetaWriter(HiveMetaCrawler):
     
-
     def _if_db_exits(self, db):
-        dbs = self.get_databases()
-        if db in dbs:
+        if db in self.meta.keys():
             return True
         else:
             return False
 
     def _if_table_exits(self, db, table):
         if self._if_db_exits(db):
-            tables = self.get_tables(db)
-            if table in tables:
+            if table in self.meta[db].keys():
                 return True
             else:
                 return False
         else:
             raise ValueError("[ERROR] This db {} does not exist.".format(db))
+
+    def if_table_exits(self, db, table):
+        if self._if_table_exits(db, table):
+            return True
+        else:
+            return False
 
     def create_table(self, db, table, script, dryrun=False):
         if self._if_table_exits(db, table):
@@ -36,3 +40,14 @@ class HiveMetaWriter(HiveMetaCrawler):
                 print("[INFO] Table {}.{} has been created successfully.".format(db, table))
                 return True
 
+    def repair_partition(self, db, table):
+        if self._if_table_exits(db, table):
+            try:
+                self.cursor.execute("MSCK REPAIR TABLE {}.{}".format(db, table))
+                time.sleep(5)
+                print("[INFO] Table {}.{} has been updated successfully.".format(db, table))
+                return True
+            except:
+                print("[ERROR] This table {}.{} can not be updated.".format(db, table))
+        else:
+            raise ValueError("[ERROR] This table {}.{} does not exists.".format(db, table))
