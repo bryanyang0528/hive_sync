cac8bfcbb8bd2c025c3190ac85c4a71712d660fa
EC2 Default User <ec2-user@ip-172-31-1-175.ap-northeast-1.compute.internal>
2017-07-24 14:06:08 +0800

    updated

--- sync_table.py
+++ sync_table.py
@@ -9,6 +9,9 @@ def run(*args, **kwargs):
     dest = kwargs.get('dest')
     dest_port = kwargs.get('destport')
     dryrun = kwargs.get('dryrun')
+    repair= kwargs.get('repair')
+    print('src={}, dest={}, dryrun={}, repair={}'.format(src, dest, dryrun, repair))
+
 
     hmc_source = HiveMetaCrawler(host=src, port=src_port)
     hmc_dest = HiveMetaWriter(host=dest, port=dest_port)
@@ -16,19 +19,26 @@ def run(*args, **kwargs):
 
     for db in meta.keys():
         for table in meta[db]:
-            try:
-                script = hmc_source.get_create_table_script(db, table)
-                hmc_dest.create_table(db, table, script, dryrun)
-            except ValueError as e:
-                print(e)
+            if hmc_dest.if_table_exits(db, table):
+                if repair:
+                    hmc_dest.repair_partition(db, table)
+            else:
+                try:
+                    script = hmc_source.get_create_table_script(db, table)
+                    hmc_dest.create_table(db, table, script, dryrun)
+                except ValueError as e:
+                    print(e)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.set_defaults(func=run)
-    parser.add_argument('-src', type=str, required=True, metavar='source hiveserver')
-    parser.add_argument('-srcport', type=int, required=False, default=10000, metavar='port of src')
-    parser.add_argument('-dest', type=str, required=True, metavar='detination hiveserver')
-    parser.add_argument('-destport', type=int, required=False, default=10000, metavar='port of dest')
-    parser.add_argument('-dryrun', type=bool, required=False, default=False, metavar='dry run')
+    parser.add_argument('-src', type=str, required=True, help='source hiveserver')
+    parser.add_argument('-srcport', type=int, required=False, default=10000, help='port of src')
+    parser.add_argument('-dest', type=str, required=True, help='detination hiveserver')
+    parser.add_argument('-destport', type=int, required=False, default=10000, help='port of dest')
+    parser.add_argument('-dryrun', type=bool, required=False, 
+                            default=False, help='set to dry run')
+    parser.add_argument('-repair', type=bool, required=False, 
+                            default=False, help='set repair partition')
     args = parser.parse_args()
     args.func(**vars(args))
